32457f28a0195e796b26644c82d2b5f2543b038d
Munima <Munima@10.0.0.141>
2021-03-24 13:33:07 -0600

    Project added

---
+++ src/testprocessor/Main.java
@@ -0,0 +1,27 @@
+package testprocessor;
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Processor!");
+    
+    Processor p = new Processor();
+    System.out.println("Enter an integer value or enter 'on'/'off' to turn the processor on/off: ");
+    Scanner s = new Scanner(System.in);
+    while (s.hasNext()) {
+      if(s.hasNextInt()) {
+        p.process(s.nextInt());
+      } else {
+        String text = s.next();
+        switch (text) {
+          case "on": p.on(); break;
+          case "off": p.off(); break;
+        }
+      }
+      System.out.println("Total is " + p.getTotal());
+    }
+    
+    
+  }
+}
